feed67e3a7ff1284d08743c88d8389a25cbb6cee
AbdulHadi-Programmer <abdulhadistmstudent@gmail.com>
2024-09-01 00:37:58 +0500

    Add new Django Project

---
+++ wscubetech/urls.py
@@ -0,0 +1,43 @@
+"""
+URL configuration for wscubetech project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+### Three types of value we can sent:
+1. int, 2. str, 3. slug
+
+"""
+
+from django.contrib import admin
+from django.urls import path
+
+from wscubetech import views
+
+urlpatterns = [
+    # We can change or create urls in this files
+    path('', views.HomePage),
+    path('admin/', admin.site.urls),
+    path('about-us/', views.aboutUs),
+    path('course/', views.course),
+    # this below will create a dynamic url with only int arg because we already defined it.
+    # path('course/<int:courseid>', views.courseDetail),
+    # path('course/<slug:courseid>', views.courseDetail),
+    # If we dont know any data come on our url then we can leave empty , = to all data types.
+    
+    # YE HAI ABHI WALA CODE AUE HTML ME FOR LOOP LEARNING KO MAT CHERNA USNE MENE DJANGO K TAGS LIKHAY HOAY HAI .
+    path('course/<courseid>', views.courseDetail),
+    path('/about/', views.aboutUs, name='about'),
+    path('/services/', views.course, name='services'),
+    path('/contact/', views.contact, name='contact'),
+
+]
---
+++ wscubetech/views.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponse # Show text on browser
+from django.shortcuts import render
+
+def HomePage(request):
+    # data = {
+    #     'title' : 'Home Page',
+    #     'bdata': 'Hello I am a Developer',
+    #     'clist': ['python', 'js', 'java', 'c', 'go', 'dart'],
+    #     # 'numbers' : [],
+    #     'numbers' : [11, 22, 33, 44, 55, 66],
+    #     'student_detail': [
+    #         {'name': 'Student 1', 'age': 19},
+    #         {'name': 'Student 2', 'age': 29}
+    #     ]
+    # }
+    return render(request, "index.html")
+
+def aboutUs(request):
+    return HttpResponse("<b>Welcome to My Website </b>")
+
+def course(request):
+    return HttpResponse("Welcome to My Website Course Page")
+
+def courseDetail(request, courseid):
+    return HttpResponse(courseid)
+
+## Testing of all Html Pages:
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def HomePage(request):
+    return render(request, 'index.html')
+
+def aboutUs(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    return render(request, 'contact.html')
